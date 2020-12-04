78ed0a84e024c0e6a8a54b249f8594476648ef1f
WoJiaoChaDi <264835229@qq.com>
2020-12-04 14:55:59 +0800

    删除无用类

--- src/main/java/com/chadi/dbcompare/test/DbCompareTest.java
+++ src/main/java/com/chadi/dbcompare/test/DbCompareTest.java
@@ -1,5 +1,6 @@
 package com.chadi.dbcompare.test;
 
+import com.alibaba.druid.util.StringUtils;
 import com.chadi.dbcompare.bean.Dba_tables;
 import com.chadi.dbcompare.dao.Dba_tablesMapper;
 import com.chadi.factory.DataSourceEnum;
@@ -8,43 +9,29 @@ import com.chadi.factory.MapperFactory;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
 
-/**
- * 1、接口式编程
- * 	原生：		Dao		====>  DaoImpl
- * 	mybatis：	Mapper	====>  xxMapper.xml
- * 
- * 2、SqlSession代表和数据库的一次会话；用完必须关闭；
- * 3、SqlSession和connection一样她都是非线程安全。每次使用都应该去获取新的对象。
- * 4、mapper接口没有实现类，但是mybatis会为这个接口生成一个代理对象。
- * 		（将接口和xml进行绑定）
- * 		EmployeeMapper empMapper =	sqlSession.getMapper(EmployeeMapper.class);
- * 5、两个重要的配置文件：
- * 		mybatis的全局配置文件：包含数据库连接池信息，事务管理器信息等...系统运行环境信息
- * 		sql映射文件：保存了每一个sql语句的映射信息：
- * 					将sql抽取出来。	
- * 
- * 
- * @author lfy
- *
- */
 public class DbCompareTest {
 	
 	@Test
 	public void test_MapperFactory() throws IOException {
 
+		final Logger logger = LoggerFactory.getLogger(DbCompareTest.class);
+
 		Dba_tablesMapper mapper = MapperFactory.createMapper(Dba_tablesMapper.class, DataSourceEnum.d1);
 
 		List<Dba_tables> dba_tablesList = mapper.getDba_tablesByOwner("HEAD");
-		System.out.println(mapper.getClass());
 
-		for (int i = 0; i < 10; i++) {
-			Dba_tables dba_tables = dba_tablesList.get(i);
-			System.out.println(dba_tables.toString());
-		}
+		logger.info(""+dba_tablesList.size());
+
+		//for (int i = 0; i < 10; i++) {
+		//	Dba_tables dba_tables = dba_tablesList.get(i);
+		//	System.out.println(dba_tables.toString());
+		//}
 	}
 
 
